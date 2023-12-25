ef1d8175df7d5228db6ef5e4804ffdd21dd7c6b3
Erfan <erfanmahvash12345@gmail.com>
2023-12-26 00:25:12 +0330

    save to db

--- index.ts
+++ index.ts
@@ -1,14 +1,28 @@
 import Crawler from "./src/crawler";
 import config from "./src/config";
+import { AppDataSource } from "./src/config/data-source";
+import ImageEntity from "./src/entity/Image";
+import imageRepo from "./src/repo/Image";
 
 async function main() {
   // setup config
-  await config.setupConfig();
+  config.setupConfig();
+  await AppDataSource.initialize();
+  console.log("DB connected");
 
   // crawl
   const crawler = new Crawler();
   const buffers = await crawler.crawlAndResize();
-  console.log(buffers);
+
+  //   save to db
+  await imageRepo.insert(
+    buffers.map((data) => {
+      const image = new ImageEntity();
+      image.data = data;
+      image.query = config.env.QUERY;
+      return image;
+    })
+  );
 }
 
 main();
